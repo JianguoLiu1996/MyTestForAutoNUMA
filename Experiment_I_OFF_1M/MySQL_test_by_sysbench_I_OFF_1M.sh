1cca1452ee746c1c97e8cf09e875ed1630df297a
liujianguo <491925717@qq.com>
2023-03-19 21:42:16 +0800

    Test result of interleave allocation and close numa balance,change set to 1M.20230319

---
+++ Experiment_I_OFF_1M/MySQL_test_by_sysbench_I_OFF_1M.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+OUTPUTPATH=/home/liujianguo/MyTestForAutoNUMA/Experiment_I_OFF_1M/
+function rwtest(){
+#	echo "==Prepare data for RW=="
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--threads=20 \
+#		--oltp-table-size=1000000 \
+#		--oltp-tables-count=64 \
+#		--mysql-db=testdb \
+#		--db-driver=mysql \
+#		--mysql-host=localhost \
+#		--mysql-port=3306 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_rw_prepare_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+	
+#	echo "==run sysbench for RW=="
+#	echo "Start time is: $(date '+%c')"
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--threads=200 \
+#		--time=1800 \
+#		--max-requests=0 \
+#		--mysql-db=testdb \
+#		--oltp-table-size=1000000 \
+#		--oltp-tables-count=64 \
+#		--db-driver=mysql \
+#		--mysql-host=localhost \
+#		--mysql-port=3306 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--report-interval=3 \
+#		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_rw_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+#	echo "End time is: $(date '+%c')"
+
+	echo "==cleanup sysbench testdb of RW=="
+	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+	 	--threads=200 \
+	 	--time=1800 \
+	 	--max-requests=0 \
+	 	--mysql-db=testdb \
+	 	--oltp-table-size=1000000 \
+		--oltp-tables-count=64 \
+	 	--db-driver=mysql \
+		--mysql-host=localhost \
+		--mysql-port=3306 \
+		--mysql-user=root \
+	 	--mysql-password=123456 \
+	 	--report-interval=3 \
+	 	--forced-shutdown=1 cleanup
+}
+
+function rotest(){
+#	echo "==Prepare data for RO=="
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#	 	--threads=20 \
+#	 	--oltp-table-size=1000000 \
+#	 	--oltp-tables-count=64 \
+#	 	--mysql-db=loadtest \
+#	 	--db-driver=mysql \
+#	 	--mysql-host=localhost \
+#	 	--mysql-port=3306 \
+#	 	--mysql-user=root \
+#	 	--mysql-password=123456 \
+#	 	--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_ro_prepare_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+
+#	echo "==run sysbench test for RO=="
+#	echo "Start time is: $(date '+%c')"
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--mysql-host=localhost \
+#		--oltp-tables-count=64 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--mysql-port=3306 \
+#		--db-driver=mysql \
+#		--oltp-table-size=1000000 \
+#		--mysql-db=loadtest \
+#		--max-requests=0 \
+#		--oltp-simple-ranges=0 \
+#		--oltp-distinct-ranges=0 \
+#		--oltp-sum-ranges=0 \
+#		--oltp-order-ranges=0 \
+#		--time=1800 \
+#		--oltp-read-only=on \
+#		--threads=200 \
+#		--report-interval=3 \
+#		--thread-init-timeout=300 \
+#		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_ro_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+#	echo "End time is: $(date '+%c')"
+	
+	echo "==cleanup sysbench testdb for RO=="
+	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--mysql-host=localhost \
+		--oltp-tables-count=64 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--mysql-port=3306 \
+		--db-driver=mysql \
+		--oltp-table-size=1000000 \
+		--mysql-db=loadtest \
+		--max-requests=0 \
+		--oltp-simple-ranges=0 \
+		--oltp-distinct-ranges=0 \
+		--oltp-sum-ranges=0 \
+		--oltp-order-ranges=0 \
+		--time=1800 \
+		--oltp-read-only=on \
+		--threads=200 \
+		--report-interval=3 \
+		--thread-init-timeout=300 \
+		--forced-shutdown=1 cleanup
+}
+
+function wotest(){
+#	echo "==Prepare data for WO=="
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#	 	--threads=20 \
+#	 	--oltp-table-size=1000000 \
+#	 	--oltp-tables-count=64 \
+#	 	--mysql-db=loadtest \
+#	 	--db-driver=mysql \
+#	 	--mysql-host=localhost \
+#	 	--mysql-port=3306 \
+#	 	--mysql-user=root \
+#	 	--mysql-password=123456 \
+#	 	--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_wo_prepare_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+	
+#	echo "== run sysbench for WO=="
+#	echo "Start time is: $(date '+%c')"
+#	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--mysql-host=localhost \
+#		--oltp-tables-count=64 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--mysql-port=3306 \
+#		--db-driver=mysql \
+#		--oltp-table-size=1000000 \
+#		--mysql-db=loadtest \
+#		--max-requests=0 \
+#		--max-time=1800 \
+#		--oltp-simple-ranges=0 \
+#		--oltp-distinct-ranges=0 \
+#		--oltp-sum-ranges=0 \
+#		--oltp-order-ranges=0 \
+#		--oltp-point-selects=0 \
+#		--threads=200 \
+#		--randtype=uniform \
+#		--report-interval=3 \
+#		--thread-init-timeout=300 \
+#		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_wo_I_OFF_1M_$(date +"%Y%m%d%H%M%S").log"
+#	echo "End time is: $(date '+%c')"
+
+	echo "== run sysbench for WO"
+       	sysbench /home/liujianguo/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--mysql-host=localhost \
+                --oltp-tables-count=64 \
+                --mysql-user=root \
+                --mysql-password=123456 \
+                --mysql-port=3306 \
+                --db-driver=mysql \
+                --oltp-table-size=1000000 \
+                --mysql-db=loadtest \
+                --max-requests=0 \
+                --max-time=1800 \
+                --oltp-simple-ranges=0 \
+                --oltp-distinct-ranges=0 \
+                --oltp-sum-ranges=0 \
+                --oltp-order-ranges=0 \
+                --oltp-point-selects=0 \
+                --threads=200 \
+                --randtype=uniform \
+                --report-interval=3 \
+                --thread-init-timeout=300 \
+                --forced-shutdown=1 cleanup
+}
+
+#start fuction
+#rwtest
+#rotest
+wotest
